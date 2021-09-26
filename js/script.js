fa4f559444a16a89647ae5080240569b7d80c615
byung1211 <byung1211@edu.sait.ca>
2021-09-26 13:36:03 -0600

    Updated for the responsive menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**
+ *  Description: This is a javascript file for CPNT 260 program at SAIT.
+ *  Created date: 2021.09.26
+ *  Created by: Byung Uk An
+ */
+
+function showMenu() {
+  var navbar = document.getElementById("menu");
+  navbar.classList.toggle("show");
+}
